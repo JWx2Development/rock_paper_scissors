92763a0828c4b7788ac97bb5d2929bd6321bcc21
JWx2Development <woottondevelopment@gmail.com>
2023-07-24 12:00:07 +0100

    comit code

---
+++ index.js
@@ -0,0 +1,19 @@
+function makeChoice(playerChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    let result;
+    if (playerChoice === computerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        result = "You win!";
+    } else {
+        result = "You lose!";
+    }
+
+    document.getElementById("result-text").innerText = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
+}
