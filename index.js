2ae01f78927bf660833413c95ac423b944dcdc12
JWx2Development <woottondevelopment@gmail.com>
2023-07-25 14:46:57 +0100

    commit

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+// Move the toggleGridClass and removeGridClass functions outside of the makeChoice function
+function toggleGridClass() {
+    const choicesContainer = document.getElementById("choices-container");
+    choicesContainer.classList.toggle("grid-cols-3");
+}
+
+function removeGridClass() {
+    const choicesContainer = document.getElementById("choices-container");
+    choicesContainer.classList.remove("grid-cols-3");
+}
+
+
+// Get the player's name from the query parameter
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const playerName = urlParams.get('name');
+
+// Set the player's name as the content of the playerName element
+document.getElementById("playerName").innerText = playerName;
+
+// Initialize scores
 let playerScore = 0;
 let computerScore = 0;
+document.getElementById("player-score").innerText = playerScore;
+document.getElementById("computer-score").innerText = computerScore;
 
 function makeChoice(playerChoice, element) {
     const choices = ['rock', 'paper', 'scissors'];
@@ -16,6 +39,15 @@ function makeChoice(playerChoice, element) {
                 img.style.display = "none";
             }
         });
+
+        // Hide other choices and remove grid class
+        document.querySelectorAll('img').forEach(img => {
+            if (img !== element) {
+                img.style.display = "none";
+            }
+        });
+        removeGridClass();
+
 
         // Show the computer's choice with animation
         const computerChoiceContainer = document.getElementById("computer-choice");
@@ -44,6 +76,8 @@ function makeChoice(playerChoice, element) {
 
         document.getElementById("result-text").innerText = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
 
+
+
         // Update the scores
         document.getElementById("player-score").innerText = playerScore;
         document.getElementById("computer-score").innerText = computerScore;
@@ -65,6 +99,12 @@ function makeChoice(playerChoice, element) {
 }
 
 function playAgain() {
+
+    // Show all choices and add grid class
+    document.querySelectorAll('img').forEach(img => {
+        img.style.display = "block";
+    });
+    toggleGridClass();
     document.querySelectorAll('img').forEach(img => {
         img.style.display = "block";
         img.style.animation = "";
@@ -80,6 +120,7 @@ function playAgain() {
     computerChoiceImg.style.opacity = "0"; // Set opacity to 0 to reset
     computerChoiceContainer.style.display = "none"; // Hide computer's choice container
 
+
     document.getElementById("result-text").innerText = "";
     document.getElementById("play-again-text").innerText = "";
     document.getElementById("play-again-button").classList.add("hidden");
@@ -94,4 +135,11 @@ function newGame() {
     document.getElementById("play-again-text").innerText = "Do you want to play again?";
     document.getElementById("play-again-button").classList.remove("hidden");
     document.getElementById("new-game-button").classList.add("hidden");
+}
+
+
+
+function finishGame() {
+    // Redirect back to the home screen (index.html)
+    window.location.href = "index.html";
 }
